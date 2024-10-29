df1a756104e845a18f9c72e367d1f7e33803bf42
Ashik045 <ashikurislam045@gmail.com>
2024-10-30 00:18:01 +0600

    like popup ui creating

--- client/components/LikePopup/LikePopup.tsx
+++ client/components/LikePopup/LikePopup.tsx
@@ -1,14 +1,19 @@
+import { UserType } from "@/types.global";
+import Image from "next/image";
+import Link from "next/link";
 import { FaTimes } from "react-icons/fa";
 import styles from "./likepopup.module.scss";
 
+import nophoto from "@/public/images/no-photo.png";
+
 interface UsersType {
-  users: UsersType[];
-  setEditPopUp: React.Dispatch<React.SetStateAction<boolean>>;
+  users: UserType[];
+  setLikePopup: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const LikePopup = ({ users, setEditPopUp }: UsersType) => {
+const LikePopup = ({ users, setLikePopup }: UsersType) => {
   const handleClose = () => {
-    setEditPopUp(false);
+    setLikePopup(false);
   };
 
   console.log(users);
@@ -20,7 +25,30 @@ const LikePopup = ({ users, setEditPopUp }: UsersType) => {
           <FaTimes />
         </span>
 
-        <p>reactors</p>
+        {users.map((user) => {
+          return (
+            <div key={user._id} className={styles.like_users}>
+              <Link href={`/profile/${user._id}`}>
+                <Image
+                  src={nophoto}
+                  height={37}
+                  width={37}
+                  alt="sociatek"
+                  className={styles.user_profile}
+                />
+              </Link>
+
+              <div className={styles.like_user_name}>
+                <p>{user.displayName}</p>
+                <p>{user.title}</p>
+              </div>
+
+              <button>
+                <Link href={`/profile/${user._id}`}>View Profile</Link>
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
