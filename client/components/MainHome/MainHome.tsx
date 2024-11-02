86dd53fdfdc42655f30d0848a886bbe054225371
Ashik045 <ashikurislam045@gmail.com>
2024-11-02 17:14:44 +0600

    logout the user and remove jwttoken if the user expire the expiration time

--- client/components/MainHome/MainHome.tsx
+++ client/components/MainHome/MainHome.tsx
@@ -1,6 +1,10 @@
 "use client";
 
+import { Context } from "@/Context/Context";
 import { QuestionType } from "@/types.global";
+import { jwtDecode } from "jwt-decode";
+import { useRouter } from "next/navigation";
+import { useContext } from "react";
 import Question from "../Question/Question";
 import QuestionComponent from "../QuestionComponent/QuestionComponent";
 import styles from "./mainhome.module.scss";
@@ -10,6 +14,21 @@ interface QuestionProp {
 }
 
 const MainHome = ({ questions }: QuestionProp) => {
+  const router = useRouter();
+  const { dispatch } = useContext(Context);
+
+  const token = localStorage.getItem("jwttoken");
+
+  if (token) {
+    const decodedToken = jwtDecode<{ exp: number }>(token);
+    const expirationTime = decodedToken.exp * 1000;
+
+    if (Date.now() > expirationTime) {
+      localStorage.removeItem("jwttoken");
+      dispatch({ type: "LOGOUT" });
+      router.push("/");
+    }
+  }
   return (
     <div className={styles.main_home}>
       <QuestionComponent />
